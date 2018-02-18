fefc43417899acf19c69f4f94f62c06ba3a836c2
guruduttperi <thegdaproject@gmail.com>
2018-02-18 21:39:41 +0530

    Initial commit

---
+++ app/src/main/java/com/madebysearde/quizone/MainActivity.java
@@ -0,0 +1,42 @@
+package com.madebysearde.quizone;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView sport;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //reverseTimer(2000,sport);
+    }
+
+
+    public void reverseTimer(int Seconds, final TextView tv) {
+
+        new CountDownTimer(Seconds * 1000 + 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+
+                int hours = seconds / (60 * 60);
+                int tempMint = (seconds - (hours * 60 * 60));
+                int minutes = tempMint / 60;
+                seconds = tempMint - (minutes * 60);
+
+                tv.setText("TIME : " + String.format("%02d", hours)
+                        + ":" + String.format("%02d", minutes)
+                        + ":" + String.format("%02d", seconds));
+            }
+
+            public void onFinish() {
+                tv.setText("Completed");
+            }
+        }.start();
+    }
+}
