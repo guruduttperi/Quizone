9ad00d92eb722fe8a17443f51b0101e4fad5d65e
guruduttperi <thegdaproject@gmail.com>
2018-02-18 22:49:04 +0530

    Fixed Navigation Bar

--- app/src/main/java/com/madebysearde/quizone/MainActivity.java
+++ app/src/main/java/com/madebysearde/quizone/MainActivity.java
@@ -1,19 +1,38 @@
 package com.madebysearde.quizone;
 
 import android.os.CountDownTimer;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mToggle;
     TextView sport;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawer = (DrawerLayout)findViewById(R.id.activity_main);
+        mToggle = new ActionBarDrawerToggle(this, mDrawer,R.string.open,R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         //reverseTimer(2000,sport);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
